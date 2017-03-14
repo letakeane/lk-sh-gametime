d7a7f54ac203f5be0e8c8b29ae1608d2167deab5
Leta Keane <letakeane+turing@gmail.com>
2017-03-13 20:30:38 -0600

    Add more tests

---
+++ game-time-starter-kit-FEm1/test/index.js
@@ -0,0 +1,72 @@
+var assert = require('chai').assert;
+var Canvas = require('./game.js');
+var Snake = require('./snake.js');
+var Food = require('./food.js');
+
+describe ('Canvas', function() {
+  it.skip('should have a width of 800px', function() {
+    assert.equal(canvas.width, 800)
+  });
+
+  it.skip('should have a height of 800px', function() {
+    assert.equal(canvas.height, 800)
+  });
+})
+
+
+
+describe ('Snake', function() {
+  it.skip('should be a function', function () {
+    assert.isFunction(Snake);
+  });
+
+  it.skip('should instantiate our good friend, Snake', function () {
+    var snake = new Snake();
+    assert.isObject(snake);
+  });
+
+  it.skip('should have an x of 395', function() {
+    var snake = new Snake();
+    assert.equal(snake.x, 395);
+  });
+
+  it.skip('should have a y of 395', function() {
+    var snake = new Snake();
+    assert.equal(snake.y, 395);
+  });
+
+  it.skip('should have a height of 10', function() {
+    var snake = new Snake();
+    assert.equal(snake.height, 10);
+  });
+
+  it.skip('should have a width of 10', function() {
+    var snake = new Snake();
+    assert.equal(snake.width, 10);
+  });
+
+  it.skip('should have a color of green', function() {
+    var snake = new Snake();
+    assert.equal(snake.color, 'rgba(74, 142, 41, 1)')
+  });
+
+  it.skip('should begin moving right on right arrow click event', function() {
+    var snake = new Snake();
+    assert.equal(snake.x, x++)
+  });
+
+  it.skip('should begin moving left on left arrow click event', function() {
+    var snake = new Snake();
+    assert.equal(snake.x, x--)
+  });
+
+  it.skip('should begin moving up on up arrow click event', function() {
+    var snake = new Snake();
+    assert.equal(snake.y, y--)
+  });
+
+  it.skip('should begin moving down on down arrow click event', function() {
+    var snake = new Snake();
+    assert.equal(snake.y, y++)
+  });
+})
