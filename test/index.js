0924eac10c86e08766ee17331eec6b0378c017bc
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-03-22 13:01:15 -0600

    Working on game over

--- test/index.js
+++ test/index.js
@@ -1,12 +1,8 @@
-// var assert = require('chai').assert;
-// var game = require('./lib/game.js');
-// var Snake = require('./lib/Snake.js');
-// var Food = require('./lib/Food.js');
-// var Segment = require('./lib/Segment.js')
-//
-//
-// repaint snake every time (push new segment into array of segments)
-//
+var assert = require('chai').assert;
+var Snake = require('../lib/Snake.js');
+var Segment = require('../lib/Segment.js')
+var Food = require('../lib/Food.js')
+
 // describe ('Canvas', function() {
 //   it.skip('should have a width of 500px', function() {
 //     assert.equal(canvas.width, 500)
@@ -16,63 +12,87 @@
 //     assert.equal(canvas.height, 500)
 //   });
 // })
-//
-//
-//
-// describe ('Snake', function() {
-//   it.skip('should be a function', function () {
-//     assert.isFunction(Snake);
-//   });
-//
-//   it.skip('should instantiate our good friend, Snake', function () {
-//     var snake = new Snake();
-//     assert.isObject(snake);
-//   });
-//
-//   it.skip('should have an x of 395', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.x, 395);
-//   });
-//
-//   it.skip('should have a y of 395', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.y, 395);
-//   });
-//
-//   it.skip('should have a height of 10', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.height, 10);
-//   });
-//
-//   it.skip('should have a width of 10', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.width, 10);
-//   });
-//
-//   it.skip('should have a color of green', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.color, 'rgba(74, 142, 41, 1)')
-//   });
-//
-//   it.skip('should move down on down arrow keydown event', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.y, 396)
-//   });
-//
-//   it.skip('should move up on up arrow keydown event', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.y, 394)
-//   });
-//
-//   it.skip('should move left on left arrow keydown event', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.x, 394)
-//   });
-//
-//   it.skip('should move right on right arrow keydown event', function() {
-//     var snake = new Snake();
-//     assert.equal(snake.x, 396)
-//   });
-//
-//
-// })
+
+describe ('Segment', function () {
+  it('should have a height of 10', function() {
+    var segment = new Segment();
+
+    assert.equal(segment.height, 10);
+  });
+
+  it('should have a width of 10', function() {
+    var segment = new Segment();
+
+    assert.equal(segment.width, 10);
+  });
+})
+
+describe ('Food', function () {
+  it('should have a height', function () {
+    var food = new Food();
+
+    assert.equal(food.height, 10);
+  })
+
+  it('should have a width', function () {
+    var food = new Food();
+
+    assert.equal(food.width, 10);
+  })
+})
+
+describe ('Snake', function() {
+  it('should be a function', function () {
+    assert.isFunction(Snake);
+  });
+
+  it('should instantiate our Snake', function () {
+    var snake = new Snake();
+
+    assert.isObject(snake);
+  });
+
+  it('should have an x of 240', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.startX, 240);
+  });
+
+  it('should have a y of 240', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.startY, 240);
+  });
+
+  it.skip('should have a color of green', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.color, 'rgba(74, 142, 41, 1)')
+  });
+
+  it.skip('should move down on down arrow keydown event', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.y, 241)
+  });
+
+  it.skip('should move up on up arrow keydown event', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.y, 394)
+  });
+
+  it.skip('should move left on left arrow keydown event', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.x, 394)
+  });
+
+  it.skip('should move right on right arrow keydown event', function() {
+    var snake = new Snake();
+
+    assert.equal(snake.x, 396)
+  });
+
+
+})
